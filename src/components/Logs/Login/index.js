1841e16a34c03e5fe60bf7737b68a040d357496a
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-03 18:47:18 +0200

    mise en form du composant de connexion

--- src/components/Logs/Login/index.js
+++ src/components/Logs/Login/index.js
@@ -1,5 +1,6 @@
 // == Import
-import '../styles.scss';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 import NavBar from '../../Home/Header/NavBar';
 
 // == Composant
@@ -7,19 +8,25 @@ function Login() {
   return (
     <div>
       <NavBar />
-      <form action="" method="get">
-        <div>
-          <input type="text" name="name" id="name" placeholder="Email" required />
-        </div>
+      <div className="login">
+        <h1 className="login__title">Veuillez vous connecter</h1>
+        <form action="" method="get" className="login__form">
+          <div>
+            <input type="text" name="name" id="name" placeholder="Email" className="login__input" required />
+          </div>
 
-        <div>
-          <input type="surname" name="email" id="surname" placeholder="Mot de passe" required />
-        </div>
-
-        <div>
-          <input type="submit" value="Connexion" />
-        </div>
-      </form>
+          <div>
+            <input type="surname" name="email" id="surname" placeholder="Mot de passe" className="login__input" required />
+          </div>
+          <p className="login__sign--text">
+            Pas de compte ? Vous pouvez vous inscrire ici : 
+            <Link to="sign" className="login__sign--btn">S'inscire</Link>
+          </p>
+          <div>
+            <button type="submit" className="login__btn">Connexion</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
