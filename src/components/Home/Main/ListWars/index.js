4d43d9139028a11c9bbb70459ad29e2151217a2e
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-04 23:03:10 +0200

    On arrive à afficher nos premières données venant du state

--- src/components/Home/Main/ListWars/index.js
+++ src/components/Home/Main/ListWars/index.js
@@ -3,16 +3,15 @@ import './styles.scss';
 import { Link } from 'react-router-dom';
 
 // == Composant
-function ListWars({ id }) {
-  console.log(id);
+function ListWars({ clan1, clan2, temps}) {
   return (
     <section className="wars">
       <Link to="bets/page" className="wars__list--link">
         <ul className="wars__list">
           <div className="wars__list--item">
             <div>
-              <li className="wars__list--li">CLAN1 contre CLAN2</li>
-              <li className="wars__list--li">Temps retant : XXX</li>
+              <li className="wars__list--li">{clan1} contre {clan2}</li>
+              <li className="wars__list--li">Temps retant : {temps}</li>
             </div>
             <button type="button" className="wars__list--btn">Parier</button>
           </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,14 +4,18 @@ import Header from 'src/components/Home/Header';
 import News from 'src/components/Home/Main/News';
 import ListWars from 'src/components/Home/Main/ListWars';
 import Footer from 'src/components/Home/Footer';
+import { useSelector } from 'react-redux';
 
 // == Composant
 function Main() {
+  const wars = useSelector((state) => state.wars.list);
   return (
     <div>
       <Header />
       <News />
-      <ListWars  /*id={id} clan1={clan1} clan2={clan2} temps={temps}*/ />
+      {wars.map((war) => (
+        <ListWars key={war.id} {...war} />
+      ))}
       <Footer />
     </div>
   );
